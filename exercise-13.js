0404b42beed811095cd565a05d9e045b03b4068e
Trayand <patradyn@gmail.com>
2019-10-17 15:31:54 +0700

    a

--- exercise-13.js
+++ exercise-13.js
@@ -40,6 +40,6 @@ function targetTerdekat(arr) {
   // TEST CASES
   console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
   console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
-  console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+  console.log(targetTerdekat(['o', ' ', ,'x', ' ', ' ', 'o', 'o', 'o', ' '])); // 1
   console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
   console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
--- exercise-15.js
+++ exercise-15.js
@@ -2,6 +2,8 @@ function groupAnimals(animals) {
     // you can only write your code here!
     var arr = animals.sort()
     arr.push('')
+
+
     var tam = []
     var cetak = []
   
@@ -10,6 +12,7 @@ function groupAnimals(animals) {
       if(arr[i][0]===arr[i+1][0]){
         tes.push(arr[i])
         tes.push(arr[i+1])
+        // arr.shift()
       }else if(arr[i][0]!==arr[i-1][0]){
         tes.push(arr[i])
       }else{
@@ -28,7 +31,7 @@ function groupAnimals(animals) {
   
   
   // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil','anjing']));
   console.log('\n')
   // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
